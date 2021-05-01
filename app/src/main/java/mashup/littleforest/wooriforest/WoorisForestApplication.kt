ee52b30588d13db0db9bc08c7aacf689cfab9cc0
dlwls5201 <dlwls5201@naver.com>
2021-05-01 12:57:01 +0900

    디버깅 로그 추가

--- app/src/main/java/mashup/littleforest/wooriforest/WoorisForestApplication.kt
+++ app/src/main/java/mashup/littleforest/wooriforest/WoorisForestApplication.kt
@@ -1,12 +1,14 @@
 package mashup.littleforest.wooriforest
 
 import android.app.Application
+import com.tistory.blackjinbase.util.Dlog
 import mashup.littleforest.wooriforest.utils.PrefUtil
 
 class WoorisForestApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Dlog.initDebug(BuildConfig.DEBUG)
         PrefUtil.init(this)
     }
 }
