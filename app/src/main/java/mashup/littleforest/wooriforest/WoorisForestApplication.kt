1bd6ef415162e201a941f6e891b412c3b32acca6
dlwls5201 <dlwls5201@naver.com>
2021-05-01 19:10:11 +0900

    카카오 로그인 작업

--- app/src/main/java/mashup/littleforest/wooriforest/WoorisForestApplication.kt
+++ app/src/main/java/mashup/littleforest/wooriforest/WoorisForestApplication.kt
@@ -1,6 +1,7 @@
 package mashup.littleforest.wooriforest
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import com.tistory.blackjinbase.util.Dlog
 import mashup.littleforest.wooriforest.utils.PrefUtil
 
@@ -8,7 +9,26 @@ class WoorisForestApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initDlog()
+        initPref()
+        initKakaoLogin()
+    }
+
+    private fun initDlog() {
         Dlog.initDebug(BuildConfig.DEBUG)
+    }
+
+    private fun initPref() {
         PrefUtil.init(this)
+    }
+
+    private fun initKakaoLogin() {
+        KakaoSdk.init(this, "2c654b1de0826cb908fb1388a84c0a9b")
+        // SDK 초기화
+        /*KakaoSDK.init(object : KakaoAdapter() {
+            override fun getApplicationConfig(): IApplicationConfig {
+                return IApplicationConfig { this@WoorisForestApplication }
+            }
+        })*/
     }
 }
--- app/src/main/java/mashup/littleforest/wooriforest/ui/fragment/LoginFragment.kt
+++ app/src/main/java/mashup/littleforest/wooriforest/ui/fragment/LoginFragment.kt
@@ -2,6 +2,9 @@ package mashup.littleforest.wooriforest.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
+import com.tistory.blackjinbase.util.Dlog
 import mashup.littleforest.wooriforest.R
 import mashup.littleforest.wooriforest.base.WFFragment
 import mashup.littleforest.wooriforest.databinding.FragmentLoginBinding
@@ -17,7 +20,25 @@ class LoginFragment : WFFragment<FragmentLoginBinding>(R.layout.fragment_login) 
 
     private fun initButton() {
         binding.btnKakaoLogin.setOnClickListener {
-            goToHobbyCheck()
+            kakaoLogin()
+        }
+    }
+
+    private fun kakaoLogin() {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Dlog.e("로그인 실패 : $error")
+            } else if (token != null) {
+                Dlog.d("로그인 성공 ${token.accessToken}")
+            }
+        }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            UserApiClient.instance.loginWithKakaoTalk(requireContext(), callback = callback)
+        } else {
+            //기본 웹 브라우저를 통해 카카오계정으로 로그인
+            UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
         }
     }
 
